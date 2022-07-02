7094f602ee7e324b3eb85b4d05c427a8a7712846
Сергей <sergzv22@rambler.ru>
2022-07-02 10:46:10 +0400

    backup
    The test controller works normally through a json request from postman

---
+++ src/main/java/ru/app/web/sao/controllers/TestController.java
@@ -0,0 +1,17 @@
+package ru.app.web.sao.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController
+{
+    @GetMapping("/{state}")
+    public boolean ReverseState(@PathVariable("state") boolean stateToReverse)
+    {
+        return !stateToReverse;
+    }
+}
---
+++ src/main/java/ru/app/web/sao/entities/TestEntity.java
@@ -0,0 +1,5 @@
+package ru.app.web.sao.entities;
+
+public class TestEntity
+{
+}
