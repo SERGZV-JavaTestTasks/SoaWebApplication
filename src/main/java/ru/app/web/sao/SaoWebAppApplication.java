972a6c411fc15c5218f32eeffa2c1a77ad376362
Сергей <sergzv22@rambler.ru>
2022-07-02 10:01:31 +0400

    starting_state

---
+++ src/main/java/ru/app/web/sao/SaoWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.app.web.sao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/app/web/sao/SaoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.web.sao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
